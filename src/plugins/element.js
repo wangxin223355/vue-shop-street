48472e175bc444f3fcf9d6e801dcf5be852d2aa1
重科前端小鑫 <2677855779@qq.com>
2020-05-17 23:20:38 +0800

    完成我的页面开发

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,4 +1,8 @@
 import Vue from 'vue'
-import { Button } from 'element-ui'
+import { Button, Avatar, Row, Col, Icon } from 'element-ui'
 
 Vue.use(Button)
+Vue.use(Avatar)
+Vue.use(Row)
+Vue.use(Col)
+Vue.use(Icon)
